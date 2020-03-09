9893524c0121ab3b34ecec6b63a02ace6f4bc0f9
Gabriel Barker <gabrielbarker@gmail.com>
2020-03-09 01:35:49 +0000

    UPDATE the entry point for demonstration

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,60 +1,27 @@
-// import FunctionAnalyzer from "./FunctionAnalyzer";
-// import AnalysisData from "./AnalysisData";
-// const path = `/Users/gbarker/git/revman/force-app/main/default/classes/${process.argv[2]}.cls`;
-// let functionAnalyzer: FunctionAnalyzer = new FunctionAnalyzer();
-// let data: AnalysisData[] = functionAnalyzer.getCodeBlockSelector(path);
-
-// data.forEach(d => d.logData());
-
-// import * as fs from "fs";
-// import FunctionAnalyzer from "./FunctionAnalyzer";
-
-// const fa = new FunctionAnalyzer();
-
-// const dirPath = "/Users/gbarker/git/revman/force-app/main/default/classes/";
-// const files = fs.readdirSync(dirPath);
-
-// files.forEach(f =>
-//   fa.getCodeBlockSelector(dirPath + f).forEach(d => {
-//     if (d.getNumberOfLines() > 200) {
-//       console.log(f);
-//       d.logData();
-//     }
-//   })
-// );
-
-import CodeSeparator from "./CodeSeparator";
-import DefaultBlockTypeAnalyzer from "./DefaultBlockTypeAnalyzer";
-import fs from "fs";
 import CodeBlockSelector from "./CodeBlockSelector";
 import CodeBlock from "./CodeBlock";
+import FileAnalyzer from "./FileAnalyzer";
 import { BlockType } from "./BlockType";
+import Display from "./Display";
 
-const path = `/Users/gbarker/SideProjects/CodeAnalyzer/src/main/CodeSeparator.ts`;
-const fileText = fs.readFileSync(path).toString();
-let codeSeparator = new CodeSeparator(fileText);
-let defaultBlockTypeAnalyzer: DefaultBlockTypeAnalyzer = new DefaultBlockTypeAnalyzer();
-const blocks = codeSeparator.getCodeBlocks();
-const codeBlockSelector = new CodeBlockSelector(blocks);
+const path = `/Users/gbarker/git/revman/force-app/main/default/classes/`;
+const directoryData = FileAnalyzer.getBlocksFromDirectory(path);
+directoryData.forEach(fileData => {
+  const blocks = fileData.getCodeBlocks();
+  const codeBlockSelector = new CodeBlockSelector(blocks);
+  const invalidClasses = codeBlockSelector
+    .withType(BlockType.ClassType)
+    .withLengthMoreThan(100)
+    .getBlocks();
+  const invalidFunctions = codeBlockSelector
+    .withType(BlockType.FunctionType)
+    .withLengthMoreThan(10)
+    .getBlocks();
 
-console.log(" LINES > 100 ".padStart(20, "-").padEnd(40, "-"));
-logBlocks(codeBlockSelector.withLengthMoreThan(100).getBlocks());
-console.log();
+  const invalidBlocks = invalidClasses.concat(invalidFunctions);
 
-console.log(" TYPE = LOOP & LENGTH > 50 ".padStart(30, "-").padEnd(40, "-"));
-logBlocks(
-  codeBlockSelector
-    .withType(BlockType.LoopType)
-    .withLengthMoreThan(50)
-    .getBlocks()
-);
-console.log();
-
-function logBlocks(blocks: CodeBlock[]) {
-  blocks.forEach(b => {
-    console.log("lines: " + b.getNumberOfLines());
-    console.log("type: " + b.getType());
-    console.log("declaration: " + b.getDeclaration());
-    console.log();
-  });
-}
+  if (invalidBlocks.length) {
+    const display = new Display(fileData.getFileName(), invalidBlocks);
+    display.print();
+  }
+});
