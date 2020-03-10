b6da419ef480eb9f635f62bd330557924e0d56e0
Gabriel Barker <gabrielbarker@gmail.com>
2020-03-10 21:24:50 +0000

    ADD a method to convert strings to BlockTypes

--- src/main/BlockType.ts
+++ src/main/BlockType.ts
@@ -5,5 +5,27 @@ export enum BlockType {
   FunctionType = "function",
   IfType = "if statement",
   LoopType = "loop",
-  CollectionType = "collection"
+  CollectionType = "collection",
+  InvalidType = "invalid type"
+}
+
+export function blockTypeOf(blockType: string): BlockType {
+  switch (blockType) {
+    case "class":
+      return BlockType.ClassType;
+    case "interface":
+      return BlockType.InterfaceType;
+    case "enum":
+      return BlockType.EnumType;
+    case "function":
+      return BlockType.FunctionType;
+    case "if statement":
+      return BlockType.IfType;
+    case "loop":
+      return BlockType.LoopType;
+    case "collection":
+      return BlockType.CollectionType;
+    default:
+      return BlockType.InvalidType;
+  }
 }
