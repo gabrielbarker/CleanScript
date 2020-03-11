778aa1170210091c2f185c5ea01247426e7ba11d
Gabriel Barker <gabrielbarker@gmail.com>
2020-03-11 00:48:03 +0000

    REMOVE fileText field to increase cohesion

--- src/main/CodeSeparator.ts
+++ src/main/CodeSeparator.ts
@@ -2,24 +2,22 @@ import CodeBlock from "./CodeBlock";
 import CodeBlockFactory from "./CodeBlockFactory";
 import DefaultBlockTypeAnalyzer from "./DefaultBlockTypeAnalyzer";
 export default class CodeSeparator {
-  private fileText: string = "";
   private codeBlocks: CodeBlock[] = [];
   private startIndices: number[] = [];
   private codeBlockFactory: CodeBlockFactory;
   private currentIndentation: number = 0;
 
   constructor(fileText: string) {
-    this.fileText = fileText;
     this.codeBlockFactory = new CodeBlockFactory(fileText, new DefaultBlockTypeAnalyzer());
-    this.findAllCodeBlocks();
+    this.findAllCodeBlocks(fileText);
   }
 
   public getCodeBlocks(): CodeBlock[] {
     return this.codeBlocks;
   }
 
-  private findAllCodeBlocks(): void {
-    this.fileText.split("").forEach((character, index) => this.handleBlockChange(character, index));
+  private findAllCodeBlocks(fileText: string): void {
+    fileText.split("").forEach((character, index) => this.handleBlockChange(character, index));
   }
 
   private handleBlockChange(character: string, index: number) {
