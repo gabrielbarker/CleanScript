5245f945b275207ea5f9da4ef4d8a7692a776280
Gabriel Barker <gabrielbarker@gmail.com>
2020-03-11 00:35:30 +0000

    REMOVE the blockTypeOf method

--- src/main/BlockType.ts
+++ src/main/BlockType.ts
@@ -8,24 +8,3 @@ export enum BlockType {
   CollectionType = "collection",
   InvalidType = "invalid type"
 }
-
-export function blockTypeOf(blockType: string): BlockType {
-  switch (blockType) {
-    case "class":
-      return BlockType.ClassType;
-    case "interface":
-      return BlockType.InterfaceType;
-    case "enum":
-      return BlockType.EnumType;
-    case "function":
-      return BlockType.FunctionType;
-    case "if statement":
-      return BlockType.IfType;
-    case "loop":
-      return BlockType.LoopType;
-    case "collection":
-      return BlockType.CollectionType;
-    default:
-      return BlockType.InvalidType;
-  }
-}
