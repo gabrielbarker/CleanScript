ac4b940aec6eee44e197e7c68bb738fb2a83db89
Gabriel Barker <gabrielbarker@gmail.com>
2020-03-13 21:12:08 +0000

    RENAME & REFACTOR the file analyzer class
    The class could previously handle dirs with files (no sub dirs)

---
+++ src/main/DataRetriever.ts
@@ -0,0 +1,42 @@
+import CodeSeparator from "./CodeSeparator";
+import fs, { readdirSync, lstatSync } from "fs";
+import FileData from "./FileData";
+
+export default class DataRetriever {
+  private fileData: FileData[] = [];
+
+  constructor(path: string) {
+    this.getDataFromPath(path);
+  }
+
+  public getFileData() {
+    return this.fileData;
+  }
+
+  private getDataFromPath(path: string) {
+    console.log(path);
+    const fileStats = lstatSync(path);
+    console.log(fileStats.isFile());
+
+    if (fileStats.isFile()) this.getDataFromFile(path);
+    else if (fileStats.isDirectory()) this.getDataFromDirectory(path);
+  }
+
+  private getDataFromDirectory(path: string): void {
+    path = this.endWithSlash(path);
+    const fileNames = readdirSync(path);
+    fileNames.forEach(fileName => this.getDataFromPath(path + fileName));
+  }
+
+  private getDataFromFile(path: string): void {
+    const fileText = fs.readFileSync(path).toString();
+    const codeSeparator = new CodeSeparator(fileText);
+    const blocks = codeSeparator.getCodeBlocks();
+    this.fileData.push(new FileData(path, blocks));
+  }
+
+  private endWithSlash(path: string) {
+    if (!path.endsWith("/")) return (path += "/");
+    return path;
+  }
+}
