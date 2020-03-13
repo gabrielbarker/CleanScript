ee923d81cfb530ba6ddf10cdd150307362f92834
Gabriel Barker <gabrielbarker@gmail.com>
2020-03-13 21:14:12 +0000

    ADD classes to handle creating templates
    This implementation is flawed and needs rethinking

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,18 +1,25 @@
-import FileAnalyzer from "./FileAnalyzer";
+import DataRetriever from "./DataRetriever";
 import Display from "./Display";
 import InvalidBlockSelector from "./InvalidBlockSelector";
+import TableData from "./table/TableData";
+import TableRenderer from "./table/TableRenderer";
 
 const codeAnalyzerPath = `/Users/gbarker/GitHub/CodeAnalyzer/src/main/`;
 const revmanPath = `/Users/gbarker/git/revman/force-app/main/default/classes/`;
 
 function printDirectoryAnalysis(dirPath: string) {
-  const directoryData = FileAnalyzer.getDataFromDirectory(dirPath);
-  directoryData.forEach(fileData => {
-    const invalidBlockSelector = new InvalidBlockSelector(fileData.getCodeBlocks());
-    const invalidBlocks = invalidBlockSelector.getBlocksOverLineLimit();
-    const display = new Display(fileData.getFileName(), invalidBlocks);
-    display.printInvalidBlocks();
-  });
+  const dataRetriever: DataRetriever = new DataRetriever(dirPath);
+  const directoryData = dataRetriever.getFileData();
+  const data = [];
+  const tableData = new TableData(directoryData);
+  const tableRenderer = new TableRenderer(tableData);
+  tableRenderer.printTable();
+  // directoryData.forEach(fileData => {
+  //   const invalidBlockSelector = new InvalidBlockSelector(fileData.getCodeBlocks());
+  //   const invalidBlocks = invalidBlockSelector.getBlocksOverLineLimit();
+  //   const display = new Display(fileData.getFileName(), invalidBlocks);
+  //   display.printInvalidBlocks();
+  // });
 }
 
-printDirectoryAnalysis(revmanPath);
+printDirectoryAnalysis(codeAnalyzerPath);
