a93c940e85bda20c73aa844dc421e0ded5e8071f
Gabriel Barker <gabrielbarker@gmail.com>
2020-03-16 01:48:41 +0000

    UTILIZE Taybl in index.ts

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,34 +1,30 @@
 import DataRetriever from "./DataRetriever";
-import Display from "./Display";
 import InvalidBlockSelector from "./InvalidBlockSelector";
 import TableData from "./table/TableData";
-import TableRenderer from "./table/TableRenderer";
 import Taybl from "taybl";
+import FileData from "./FileData";
 
 const codeAnalyzerPath = `/Users/gbarker/GitHub/CodeAnalyzer/src/main/`;
 const revmanPath = `/Users/gbarker/git/revman/force-app/main/default/classes/`;
 
 function printDirectoryAnalysis(dirPath: string) {
   const dataRetriever: DataRetriever = new DataRetriever(dirPath);
-  const directoryData = dataRetriever.getFileData();
-  const data = [];
+  let directoryData = dataRetriever.getFileData();
+  directoryData = directoryData.map(fileData => {
+    const selector = new InvalidBlockSelector(fileData.getCodeBlocks());
+    return new FileData(fileData.getFileName(), selector.getBlocksOverLineLimit());
+  });
   const tableData = new TableData(directoryData);
-
   const taybl = new Taybl(tableData);
   taybl
     .withHorizontalLineStyle("=")
-    .withVerticalLineStyle(":")
+    .withVerticalLineStyle("||")
     .withNumberOfSpacesAtStartOfColumns(2)
     .withNumberOfSpacesAtEndOfColumns(2)
     .print();
-  // const tableRenderer = new TableRenderer(tableData);
-  // tableRenderer.printTable();
-  // // directoryData.forEach(fileData => {
-  // //   const invalidBlockSelector = new InvalidBlockSelector(fileData.getCodeBlocks());
-  // //   const invalidBlocks = invalidBlockSelector.getBlocksOverLineLimit();
-  // //   const display = new Display(fileData.getFileName(), invalidBlocks);
-  // //   display.printInvalidBlocks();
-  // // });
 }
 
-printDirectoryAnalysis(codeAnalyzerPath);
+const before = Date.now();
+printDirectoryAnalysis(revmanPath);
+const after = Date.now();
+console.log(after - before);
