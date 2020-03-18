a0d53058cdb364ebdb9d8a09eb24f7b20216cd06
Gabriel Barker <gabrielbarker@gmail.com>
2020-03-18 11:43:31 +0000

    ADD typeLimits functionality to the demo

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,22 +1,40 @@
 import DataRetriever from "./DataRetriever";
-import InvalidBlockSelector from "./InvalidBlockSelector";
-import TableDataFormatter from "./TableDataFormatter";
+import LineLimitBlockSelector from "./LineLimitBlockSelector";
+import LineLimitTableFormatter from "./LineLimitTableFormatter";
+import TypeLimitBlockSelector from "./TypeLimitBlockSelector";
+import TypeLimitTableFormatter from "./TypeLimitTableFormatter";
 import Taybl from "taybl";
 import FileData from "./FileData";
 
 const codeAnalyzerPath = `/Users/gbarker/GitHub/CodeAnalyzer/src/main/`;
 const revmanPath = `/Users/gbarker/git/revman/force-app/main/default/classes/`;
+
 function printDirectoryAnalysis(dirPath: string) {
   const dataRetriever: DataRetriever = new DataRetriever(dirPath);
   let directoryData = dataRetriever.getFileData();
   directoryData = directoryData.map(fileData => {
-    const selector = new InvalidBlockSelector(fileData.getCodeBlocks());
+    const selector = new LineLimitBlockSelector(fileData.getCodeBlocks());
     return new FileData(fileData.getFileName(), selector.getBlocksOverLineLimit());
   });
-  const tableData = new TableDataFormatter(directoryData).getTableData();
 
-  const taybl = new Taybl({ files: tableData });
-  taybl
+  const lineLimitData = new LineLimitTableFormatter(directoryData).getTableData();
+  const lineLimitTaybl = new Taybl({ files: lineLimitData });
+  lineLimitTaybl
+    .withHorizontalLineStyle("=")
+    .withVerticalLineStyle(":")
+    .withNumberOfSpacesAtStartOfColumns(2)
+    .withNumberOfSpacesAtEndOfColumns(2)
+    .print();
+
+  directoryData = dataRetriever.getFileData();
+  directoryData = directoryData.map(fileData => {
+    const selector = new TypeLimitBlockSelector(fileData.getCodeBlocks());
+    return new FileData(fileData.getFileName(), selector.getBlocksOverTypeLimit());
+  });
+
+  const typeLimitData = new TypeLimitTableFormatter(directoryData).getTableData();
+  const typeLimitTaybl = new Taybl({ files: typeLimitData });
+  typeLimitTaybl
     .withHorizontalLineStyle("=")
     .withVerticalLineStyle(":")
     .withNumberOfSpacesAtStartOfColumns(2)
