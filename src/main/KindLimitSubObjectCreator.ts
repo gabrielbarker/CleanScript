d7008b38b484aec5dd4eb8d8b9861fd861a617a2
Gabriel Barker <gabrielbarker@gmail.com>
2020-03-22 20:14:26 +0000

    RENAME a class

--- src/main/KindLimitSubObjectCreator.ts
+++ src/main/KindLimitSubObjectCreator.ts
@@ -1,7 +1,7 @@
 import CodeBlock from "./CodeBlock";
 import SubObjectCreator from "./SubObjectCreator";
 
-export default class KindObjectCreator implements SubObjectCreator {
+export default class KindLimitSubObjectCreator implements SubObjectCreator {
   public createObject(blocks: CodeBlock[]): any {
     return { kind: blocks[0].kind, count: blocks.length };
   }
