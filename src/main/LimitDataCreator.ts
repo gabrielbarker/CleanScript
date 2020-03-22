24fa6e0fb523b0166bc214678a824aa91231c824
Gabriel Barker <gabrielbarker@gmail.com>
2020-03-22 20:16:15 +0000

    EXTRACT the dependency of limit selection
    A limit selector is now passed in to select blocks to be displayed

--- src/main/LimitDataCreator.ts
+++ src/main/LimitDataCreator.ts
@@ -1,7 +1,7 @@
 import CodeBlock from "./CodeBlock";
 import CodeBlockSelector from "./CodeBlockSelector";
-import KindObjectCreator from "./KindLimitSubObjectCreator";
 import SubObjectCreator from "./SubObjectCreator";
+import LimitSelector from "./LimitSelector";
 
 type FileObject = { fileName: string; invalid: KindObject[] };
 type KindObject = { kind: string; count: number; "line numbers": string };
@@ -9,12 +9,19 @@ type KindObject = { kind: string; count: number; "line numbers": string };
 export default class LimitDataCreator {
   private blocks: CodeBlock[];
   private fileObjects: FileObject[] = [];
+  private limitSelector: LimitSelector;
   private subObjectCreator: SubObjectCreator;
 
-  constructor(blocks: CodeBlock[], subObjectCreator: SubObjectCreator) {
+  constructor(
+    blocks: CodeBlock[],
+    limitSelector: LimitSelector,
+    subObjectCreator: SubObjectCreator
+  ) {
     this.blocks = blocks;
+    this.limitSelector = limitSelector;
     this.subObjectCreator = subObjectCreator;
     this.createFileObjects();
+    this.filterFileObjects();
   }
 
   public getTayblData(): any {
@@ -30,14 +37,13 @@ export default class LimitDataCreator {
     });
   }
 
+  private filterFileObjects(): void {
+    this.fileObjects = this.fileObjects.filter(fileObject => fileObject.invalid.length);
+  }
+
   private getAllFileNames(): string[] {
     const namesSet = new Set<string>(this.blocks.map(block => block.fileName));
     return [...namesSet];
-  }
-
-  private getAllKindsFor(blocks: CodeBlock[]): string[] {
-    const kindSet = new Set<string>(blocks.map(block => block.kind));
-    return [...kindSet];
   }
 
   private createFileObject(blocks: CodeBlock[]): any {
@@ -46,11 +52,19 @@ export default class LimitDataCreator {
     return { fileName: blocks[0].fileName, invalid: this.getAllKindObjects(kinds, selector) };
   }
 
+  private getAllKindsFor(blocks: CodeBlock[]): string[] {
+    const kindSet = new Set<string>(blocks.map(block => block.kind));
+    return [...kindSet];
+  }
+
   private getAllKindObjects(kinds: string[], selector: CodeBlockSelector) {
-    return kinds.map(kind => {
-      const blocksWithSameKind = selector.withKind(kind).getBlocks();
-      return this.createKindObject(blocksWithSameKind);
-    });
+    return kinds.map(kind => this.getKindObject(kind, selector)).filter(obj => obj);
+  }
+
+  private getKindObject(kind: string, selector: CodeBlockSelector): KindObject | undefined {
+    const blocksWithSameKind = selector.withKind(kind).getBlocks();
+    const invalidBlocks = this.limitSelector.getBlocks(blocksWithSameKind);
+    if (invalidBlocks.length) return this.createKindObject(invalidBlocks);
   }
 
   private createKindObject(blocks: CodeBlock[]): KindObject {
