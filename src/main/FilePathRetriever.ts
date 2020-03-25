1d49b8b222dce465be4809b95bfd4efdeb35cd46
Gabriel Barker <gabrielbarker@gmail.com>
2020-03-25 01:51:52 +0000

    ADD the glob/path logic to the FilePathRetriever

--- src/main/FilePathRetriever.ts
+++ src/main/FilePathRetriever.ts
@@ -1,7 +1,15 @@
 import { sync } from "glob";
+import GlobRetriever from "./GlobRetriever";
+import { existsSync } from "fs";
 
 export default class FilePathRetriever {
-  public getFilePaths(glob: string): string[] {
+  public getFilePaths(globOrPath: string): string[] {
+    const glob = existsSync(globOrPath) ? GlobRetriever.getGlob(globOrPath) : globOrPath;
+    const paths = this.getFilePathsFromGlob(glob);
+    return paths;
+  }
+
+  private getFilePathsFromGlob(glob: string): string[] {
     const options = { absolute: true };
     return sync(glob, options);
   }
